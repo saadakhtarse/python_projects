e8a8d2482e85a6f4bf13f0eaf8dd9ea59e32befb
Saad Akhtar <98681830+saadakhtarse@users.noreply.github.com>
2025-08-19 05:28:08 +0500

    Update app.py

--- app.py
+++ app.py
@@ -77,6 +77,7 @@ def find_schools():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    # app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
     # app.run(host="0.0.0.0", port=port)
     # app.run(debug=False, use_reloader=False)
+    app.run(host="0.0.0.0", port=5000)
